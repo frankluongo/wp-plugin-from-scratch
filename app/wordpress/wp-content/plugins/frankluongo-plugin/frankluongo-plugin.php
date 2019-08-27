8a35379d1f660043a2217814a9c1a56a97e1460c
frankluongo <frank@frankluongo.com>
2019-08-27 08:32:55 -0600

    Define minimum plugin settings

---
+++ app/wordpress/wp-content/plugins/frankluongo-plugin/frankluongo-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package frankluongoplugin
+ */
+/*
+  Plugin Name: Frankluongo Plugin
+  Plugin URI: https://frankluongo.com
+  Description: My first Plugin
+  Version: 1.0.0
+  Author: Frank Luongo
+  Author URI: https://frankluongo.com
+  License: GPLv2 or later
+  Text Domain: frankluongo-plugin
+*/
