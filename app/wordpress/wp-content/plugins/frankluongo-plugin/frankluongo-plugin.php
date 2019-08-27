dc096a414d19b4f61fdf74478ce076c6e20bd6d4
frankluongo <frank@frankluongo.com>
2019-08-27 08:59:41 -0600

    OOP in PHP + WP Methods

--- app/wordpress/wp-content/plugins/frankluongo-plugin/frankluongo-plugin.php
+++ app/wordpress/wp-content/plugins/frankluongo-plugin/frankluongo-plugin.php
@@ -13,7 +13,42 @@
   Text Domain: frankluongo-plugin
 */
 
-/*
-  FIRST: Check to see that Wordpress is defined before doing anything
-*/
-defined('ABSPATH') or die('Wordpress is not active');
+
+// FIRST: Check to see that Wordpress is defined before doing anything
+  defined('ABSPATH') or die('Wordpress is not active');
+
+// SECOND: Create your Plugin Class
+class FrankluongoPlugin {
+  // function __construct() {
+  // }
+
+  function activate() {
+    // ex. Generate a custom Post Type
+    // ex. Flush Rewrite Rules
+    echo 'The plugin was activated';
+  }
+
+  function deactivate() {
+    // ex. Flush Rewrite Rules
+    echo 'The plugin was deactivated';
+  }
+
+  function uninstall() {
+    // ex. Delete Custom Post Type
+
+  }
+}
+
+// THIRD: Make sure Class exists, and if it does, call it
+if (class_exists( 'FrankluongoPlugin' ) ) {
+  $frankluongoPlugin = new FrankluongoPlugin();
+}
+
+//  Wordpress has 3 built-in events that occur within a plugin lifecycle
+  // Activation
+  register_activation_hook(__FILE__, array($frankluongoPlugin, 'activate'));
+  // Deactivation
+  register_deactivation_hook(__FILE__, array($frankluongoPlugin, 'deactivate'));
+  // Uninstallation
+  // Coming later...
+
