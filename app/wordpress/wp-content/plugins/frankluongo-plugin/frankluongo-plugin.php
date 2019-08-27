423ddd8ee93ad1dafb78d3e24ec529312a3bc352
frankluongo <frank@frankluongo.com>
2019-08-27 08:43:29 -0600

    Pick a way to safeguard the plugin

--- app/wordpress/wp-content/plugins/frankluongo-plugin/frankluongo-plugin.php
+++ app/wordpress/wp-content/plugins/frankluongo-plugin/frankluongo-plugin.php
@@ -15,20 +15,5 @@
 
 /*
   FIRST: Check to see that Wordpress is defined before doing anything
-
-  If Absolute Path is not defined, kill this plugin
-  Abs Path is created by WP, so if it doesn't exist then someone is trying to indirectly access the site
-  This can also be performed with add action
 */
-// Option 1
-if (!defined('ABSPATH')) {
-  die;
-}
-
-// Option 2
 defined('ABSPATH') or die('Wordpress is not active');
-
-// Option 3
-if (!function_exists('add_action')) {
-  exit;
-}
