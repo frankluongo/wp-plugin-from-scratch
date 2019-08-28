d871945bd616dce3deeef02ed2a229d973b74b6d
frankluongo <frank@frankluongo.com>
2019-08-27 20:38:47 -0600

    Add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
 const webpack = require('webpack');
 const path = require('path');
+
+const paths = {
+  src: './plugin/index.js',
+  dist: path.resolve(
+    __dirname,
+    'app/wordpress/plugins/frankluongo-plugin/assets',
+  ),
+};
+
 const config = {
-  entry: './src/index.js',
+  entry: paths.src,
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: paths.dist,
+    filename: 'frankluongo-plugin.js.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
   },
 };
 module.exports = config;
